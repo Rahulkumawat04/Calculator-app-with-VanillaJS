d73e73329c49336ee80efc2652b1796860f4cb76
Rahul Kumawat <mailrahul2004@gmail.com>
2021-06-19 13:29:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// ALL VARIABLES
+
+// input box
+var input1 = document.querySelector("#input-number-1");
+var input2 = document.querySelector("#input-number-2");
+
+// buttons
+var add = document.querySelector("#add");
+var sub = document.querySelector("#sub");
+var multiply = document.querySelector("#multi");
+var divide = document.querySelector("#divide");
+
+// outputdiv
+var outputDiv =  document.querySelector("#output");
+
+//Processing part
